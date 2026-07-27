82df2e1271afaf2220831b20abae910c26181b40
Andres Jacinto <ahndrsx@gmail.com>
2026-07-27 14:44:15 -0600

    new deploy test

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,14 @@ import ParticleBackground from "./components/Backgound";
 import { AuthProvider } from "./context";
 import { AppRouter } from "./router/AppRouter";
 
-
-
-
-
 export default function App() {
   return (
-    
     <AuthProvider>
-      
-      <ParticleBackground/>
-      <AppRouter/>
-      <hr className="footer-hr"/><br/>
-      <ATFooter/>      
+      <ParticleBackground />
+      <AppRouter />
+      <hr className="footer-hr" />
+      <br />
+      <ATFooter />
     </AuthProvider>
-  )
+  );
 }
-
-
